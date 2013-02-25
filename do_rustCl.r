c64de6ca8470fc71091e52ce03868314998ffa85
Anas A Rana <anas.rana@gmail.com>
2013-02-25 08:47:27 +0100

    - moving the files that are used for specific sim and fittings to ../run - only leaving actual rust code files in the repo

--- do_rustCl.r
+++
@@ -1,8 +0,0 @@
-remove(list=ls())
-load('../data/sandraDat.rdat')
-source('func_rust.r')
-
-tmp <- rust.clst.fit(gData=dat$g[dat$ind,], tData=dat$t, lambda=0.05, n.states=3, fit.as='log2Dat', rSmpls=5, fit.pll=F)
-str(tmp)
-
-save(tmp, file='test.rdat')
