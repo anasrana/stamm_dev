a15e8347a2aeed5ff81f1e38ef79ea68efd4e021
Anas A Rana <anas.rana@gmail.com>
2013-02-26 10:32:39 +0100

    - slightly rewritten the plotting and return ggplot instead of just plotting it - addid viewport function to allow grid plot

--- auxfunc.r
+++ auxfunc.r
@@ -1,10 +1,10 @@
 require(ggplot2)
 require(grid)
-source('func_rust.r')
 
-##' .. content for \description{} (no empty lines) ..
+##' Function to show beta values for sim next to beta values for fit
+##' 
 ##'
-##' .. content for \details{} ..
+##' 
 ##' @title rust.comp.B
 ##' @param b.sim beta matrix that was used in the simulation
 ##' @param b.fit beta matrix from fitting
@@ -16,28 +16,29 @@ rust.comp.B <- function(b.sim, b.fit){
   k <- ncol(b.sim)
   b.val <- data.frame(beta=c(as.vector(b.sim), as.vector(b.fit)), tp=rep(c('sim', 'fit'), each=p*k), stt=rep(1:k, 2*p), gn=rep(c(rownames(b.sim), rownames(b.fit)), each=k))
 
-  ggplot(b.val) +
+b.g <- ggplot(b.val) +
     geom_bar(aes(x=stt, y=beta, alpha=factor(tp), fill=factor(stt)),position='dodge', stat='identity') +
       scale_alpha_discrete(range=c(0.5,1)) +
         xlab('States')+
-        ylab('beta value') +
-        facet_wrap(~gn, scales='free_y') +
-          theme_bw() +
-            labs(fill='States', alpha='') +
-            theme(legend.key.size=unit(0.3, 'cm'),
-  legend.text = element_text(size=10, face='bold'),
-  axis.title.x = element_text(face='bold', size=20),
-  axis.title.y = element_text(face='bold', size=20),
-                  strip.text.x = element_text(size=12),
-                  strip.background = element_rect(colour = NA),
-  axis.text.x = element_text(size=12),
-  axis.text.y = element_text(size=12))
+          ylab('beta value') +
+            facet_wrap(~gn, scales='free_y') +
+              theme_bw() +
+                labs(fill='States', alpha='') +
+                  theme(legend.key.size=unit(0.3, 'cm'),
+                        legend.text = element_text(size=10, face='bold'),
+                        axis.title.x = element_text(face='bold', size=20),
+                        axis.title.y = element_text(face='bold', size=20),
+                        strip.text.x = element_text(size=12),
+                        strip.background = element_rect(colour = NA),
+                        axis.text.x = element_text(size=12),
+                        axis.text.y = element_text(size=12))
+  return(b.g)
 }
 
 
-##' .. content for \description{} (no empty lines) ..
+##' Function to plot trajectories of data and fit on top of each other
 ##'
-##' .. content for \details{} ..
+##' 
 ##' @title rust.comp.traj
 ##' @param g.dat data used in fitting
 ##' @param t time points of g.dat
@@ -58,7 +59,7 @@ rust.comp.traj <- function(g.dat, t, b.fit, w){
   sim.dat <- data.frame(g=as.vector(g.dat), gn = rep(rownames(b.fit), length(t)),
                         t=rep(t, each=p))
 
-  ggplot(sim.dat, aes(x=t, y=g)) +
+  t.g <- ggplot(sim.dat, aes(x=t, y=g)) +
     geom_point(size=1.5, col='darkgreen') +
       geom_line(size=0.2, col='darkgreen') +
         geom_line(data=fit.dat, aes(x=t, y=g), col='blue') +
@@ -74,5 +75,7 @@ rust.comp.traj <- function(g.dat, t, b.fit, w){
                   strip.background = element_rect(colour = NA),
   axis.text.x = element_text(size=12),
   axis.text.y = element_text(size=12))
-
+  return(t.g)
 }
+
+vplayout <- function(x, y)viewport(layout.pos.row = x, layout.pos.col =y)
