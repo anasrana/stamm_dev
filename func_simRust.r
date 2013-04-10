03f7a69bbc938eef88d72681d3bf41a79fed2180
Anas A Rana <anas.rana@gmail.com>
2013-04-10 16:47:00 +0200

    - changed variable names to distinguish from function names - took jumpt time draw outside of the function to allow different types of noise in the next iteration

--- func_simRust.r
+++ func_simRust.r
@@ -1,8 +1,4 @@
 library(stats)
-## library(pracma)                       #library for lsqnonlin
-## library(Matrix)                       #matrix specific functions
-library(matrixStats)                  #use rowSds from library
-#library(pls)                            #use stdize
 library(BB)                             #use for spg
 library(expm)
 
@@ -11,65 +7,62 @@ library(expm)
 ##' Forward simulation of model using single cells
 ##'
 ##' @title RustSim
-##' @param nCells number of cells used in simulation (default 200)
-##' @param nGenes number of genes simulated (default 9)
-##' @param tau average jumpTime per state
-##' @param nStates number of states simulated (default 2)
-##' @param betaVec predefined beta values for simulation (default randomly generated)
+##' @param n.cells number of cells used in simulation (default 200)
+##' @param n.genes number of genes simulated (default 9)
+##' @param tau average jump.time per state
+##' @param n.states number of states simulated (default 2)
+##' @param beta.vec predefined beta values for simulation (default randomly generated)
 ##' @param dt timestep used throughout simulation
-##' @param endTime final timepoint (in "real" time)
-##' @param avNoise standard deviation of Gaussian noise added to the average trajectory (default 0.001)
-##' @param sttNoise vector of standard deviations for Gaussian noise per state (default 0)
-RustSim <- function(nCells = 200,nGenes = 9, tau = c(3.5,5,14.5), nStates = 2,
-                    betaVec = NULL, dt = 0.01, endTime = 30, avNoise = 0.001,
-                    sttNoise = rep(0, nStates)){
+##' @param end.time final timepoint (in "real" time)
+##' @param av.noise standard deviation of Gaussian noise added to population average (default 0.01)
+##' @param stt.noise vector of standard deviations for Gaussian noise per state (default 0)
+RustSim <- function(n.cells = 200, n.genes = 9, tau = c(3.5, 5, 14.5), n.states = 2,
+                    beta.vec = NULL, dt = 0.01, end.time = 30, av.noise = 0.01,
+                    stt.noise = rep(0, n.states)) {
 
   ## Checking if some of the arguments passed are the right format and adjusting if possible
-  if (!is.vector(betaVec) & !is.null(betaVec)) {
+  if (!is.vector(beta.vec) & !is.null(beta.vec)) {
     stop('beta must be passed as vector')
   }
-  if (length(sttNoise) != nStates & is.vector(sttNoise)) {
-    sttNoise <- rep(sttNoise, nStates)
+  if (length(stt.noise) != n.states & is.vector(stt.noise)) {
+    stt.noise <- rep(stt.noise, n.states)
   }
   ## get jump time from the state occupation time $/tau$
-  jumpTime <- NULL
-  for (i in 1:(nStates - 1)) {
-    jumpTime <- rbind(jumpTime, rexp(nCells, 1 / tau[i]))
-  }
+  jump.time <- JumpTime(tau, n.states, n.cells)
   ## Checks if beta values are passed in the argument (as vector)
   ## Assigns randomvalues to beta if not passed as argument
-  if (is.null(betaVec)) {
-    betaVec <- rnorm(nGenes * nStates, sd = 5)
-    betaVec[betaVec < 0]  <- 10^(-40)
+  if (is.null(beta.vec)) {
+    beta.vec <- rnorm(n.genes * n.states, sd = 5)
+    beta.vec[beta.vec < 0]  <- 10^(-40)
   }
   ## Reshape betavector as matrix/
-  betaVals <- matrix(betaVec, nGenes, nStates)
+  betaVals <- matrix(beta.vec, n.genes, n.states)
   ## Initialise results matrix with zeros
-  gSim <- matrix(rep(0, nGenes * endTime / dt), nGenes, endTime / dt)
-  nPoints <- endTime / dt
-  for (iCells in 1:nCells) {
+  gSim <- matrix(rep(0, n.genes * end.time / dt), n.genes, end.time / dt)
+  nPoints <- end.time / dt
+  for (iCells in 1:n.cells) {
     gSimC <- NULL
-    for (jStates in 1:(nStates - 1)) {
-      nSentries <- ceiling(jumpTime[jStates, iCells] / dt)
+    for (jStates in 1:(n.states - 1)) {
+      nSentries <- ceiling(jump.time[jStates, iCells] / dt)
       val_tmp <- betaVals[, rep.int(jStates, nSentries)]
-      gSimC <- cbind(gSimC,  val_tmp + rnorm(length(val_tmp), sd = sttNoise[jStates]) )
+      gSimC <- cbind(gSimC,  val_tmp + rnorm(length(val_tmp), sd = stt.noise[jStates]) )
       rm(val_tmp)
     }
     nSentries <- (nPoints - ncol(gSimC))
     if (nSentries > 0) {
-      val_fin <- betaVals[, rep.int(nStates, nSentries)]
-      gSimC <- cbind(gSimC, val_fin + rnorm(length(val_fin), sd = sttNoise[nStates]))
+      val_fin <- betaVals[, rep.int(n.states, nSentries)]
+      gSimC <- cbind(gSimC, val_fin + rnorm(length(val_fin), sd = stt.noise[n.states]))
       rm(val_fin)
     }
-    gSim <- gSim + gSimC[, 1:nPoints] / nCells
+    gSim <- gSim + gSimC[, 1:nPoints] / n.cells
   }
   ## Add gaussian noise to the data
-  datasim <- log2(gSim) + matrix(rnorm(length(gSim), sd=avNoise), dim(gSim))
+  datasim <- log2(gSim) + matrix(rnorm(length(gSim), sd = av.noise), dim(gSim))
   dataSim <- 2^(datasim)
   ## Return values are full simulated data all time points, beta and if t given gSim
   ## with t-pts return all parameters used for this simulation
-  return(list(gsim = gSim, beta = betaVals, dataSim = dataSim, n.cells = nCells, n.gns = nGenes,
-              tau = tau, dt = dt, n.stt = nStates, ns.av = avNoise))
+  return(list(gsim = gSim, beta = betaVals, dataSim = dataSim, n.cells = n.cells, n.gns = n.genes,
+              tau = tau, dt = dt, n.stt = n.states, ns.av = av.noise))
 }
 
 ##'  Function that add noise to normal data
@@ -81,7 +74,7 @@ RustSim <- function(nCells = 200,nGenes = 9, tau = c(3.5,5,14.5), nStates = 2,
 ##' @param gData simulated "expression" data
 ##' @return datasim
 ##' @author anas ahmad rana
-AddNoise <- function(sim = NULL, ns.sd, ns.type, gData = NULL){
+AddNoise <- function(sim = NULL, ns.sd, ns.type, gData = NULL) {
   if (is.null(gData) & exists('gsim', where = sim) )
     gData <- sim$gsim
 
@@ -96,4 +89,21 @@ AddNoise <- function(sim = NULL, ns.sd, ns.type, gData = NULL){
     datasim <- gData + matrix(rnorm(length(gData), sd = ns.sd), dim(gData))
   }
   return(datasim)
+}
+
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title JumpTime
+##' @param tau average transition times
+##' @param n.states number of states simulated
+##' @param n.cells number of cells simulated
+##' @return jump.time
+##' @author anas ahmad rana
+JumpTime  <- function(tau, n.states, n.cells) {
+  jump.time <- NULL
+  for (i in 1:(n.states - 1)) {
+    jump.time <- rbind(jump.time, rexp(n.cells, 1 / tau[i]))
+  }
+  return(jump.time)
 }
