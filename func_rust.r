1ec9186af9637e07b51c09a5b7968abe4ba1e63f
anasrana <anas.rana@gmail.com>
2013-06-25 13:59:07 +0200

    renamed variable

--- func_rust.r
+++ func_rust.r
@@ -415,18 +415,18 @@ RustFitGns <- function(g.names=NULL, g.dat, t.dat, lambda=0, n.states, w, pll=FA
     return(list(beta=betas, rss.v=rss.v, w=w, ms=ms))
 }
 
-FixedParCV <- function(g.dat, t.dat, lambda=0, n.states, m, n.core=30, t.ko=2:length(t.dat)) {
+FixedParCV <- function(g.dat, t.dat, lambda=0, k.stt, m, n.core=30, t.ko=2:length(t.dat)) {
     fit.cl <- vector('list', length(t.ko))
     fit.gn <- vector('list', length(t.ko))
     for (i.t in 1:length(t.ko)) {
-        cat(paste('time deletion ', i.t, '& k,', n.states, 'm', m, 'lambda', lambda, '...'))
+        cat(paste('time deletion ', i.t, '& k,', k.stt, 'm', m, 'lambda', lambda, '...'))
         g.dat.t <- g.dat[, -t.ko[i.t]]
         g.sd <- apply(g.dat.t, 1, sd)
         g.norm <- g.dat.t / g.sd
         g.km <- kmeans(g.norm, m, iter.max=100, nstart=50)
-        fit.cl[[i.t]] <- RustFitKstt(g.km$centers, t.dat[-t.ko[i.t]], lambda=0, n.states=k.stt)
+        fit.cl[[i.t]] <- RustFitKstt(g.km$centers, t.dat[-t.ko[i.t]], lambda=0, k.stt=k.stt)
         w <- fit.cl[[i.t]]$w
-        fit.gn[[i.t]] <- RustFitGns(g.dat=g.dat.t, t.dat=t.dat[-t.ko[i.t]], n.states=k.stt,
+        fit.gn[[i.t]] <- RustFitGns(g.dat=g.dat.t, t.dat=t.dat[-t.ko[i.t]], k.stt=k.stt,
                                     w=w, pll=TRUE, n.core=n.core)
         print('... DONE')
     }
